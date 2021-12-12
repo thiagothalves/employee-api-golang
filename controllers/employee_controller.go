f2367605826d09258334ab9bb6caa2125bcc708e
Thiago Alves do Nascimento <33322434+thiagothalves@users.noreply.github.com>
2021-12-12 10:04:41 -0300

    add core

--- controllers/employee_controller.go
+++ controllers/employee_controller.go
@@ -1,9 +1,24 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thiagothalves/employee-api-golang/core"
+)
 
 func FindAllEmployees(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+
+	employees := [2]core.Employee{
+		{
+			Id:     1,
+			Name:   "Thiago",
+			Office: "Developer",
+		},
+		{
+			Id:     2,
+			Name:   "Marcela",
+			Office: "Developer",
+		},
+	}
+
+	c.JSON(200, employees)
 }
---
+++ core/employee.go
@@ -0,0 +1,7 @@
+package core
+
+type Employee struct {
+	Id     uint   `json:"id"`
+	Name   string `json:"name"`
+	Office string `json:"office"`
+}
